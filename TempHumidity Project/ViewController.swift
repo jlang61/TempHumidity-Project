f7fb263d495d784079870e31f6dcca9f397a5c21
Justin Lang <jlang61@ucsb.edu>
2022-08-29 16:28:28 -0700

    Initial Commit

---
+++ TempHumidity Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TempHumidity Project
+//
+//  Created by Justin Lang on 8/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
